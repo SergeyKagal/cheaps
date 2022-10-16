7b9f52e1794e140dd52375ba88764f4e22730ea2
SergeyKagal <3461995@gmail.com>
2022-10-16 15:16:08 +0300

    feat: add tags from input and from list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { getData } from './getData';
 import { Chip } from '@mui/material';
-import './App.css';
+import './App.scss';
 
 function App() {
   const [tags, setTag] = useState([]);
   const [text, setText] = useState('');
+  const [tagsToSend, setTagsToSend] = useState([]);
+
+  const inputRef = useRef();
+  const divRef = useRef();
+  // const ulRef = useRef();
+
+  // const [ulWidth, setUlWidth] = useState(0);
+  const [inputWidth, setInputWidth] = useState(0);
+  const [divWidth, setDivWidth] = useState(0);
 
   const onChangeInput = (e) => {
     setText(e.target.value);
   };
+
+  const resize = () => {
+    setDivWidth(divRef.current.getBoundingClientRect().width);
+    setInputWidth(inputRef.current.getBoundingClientRect().width);
+  };
+
+  useEffect(() => {
+    resize();
+    console.log(divWidth, inputWidth);
+  });
 
   useEffect(() => {
     getData().then((responce) => {
@@ -25,36 +44,39 @@ function App() {
   const handleDelete = () => {};
 
   const onLiClick = (id) => {
+    const res = tagsToSend;
     setTag(
       [...tags].map((e) => {
         if (id !== e.id) {
           return e;
         }
+        res.push({ ...e, isClicked: true });
+        setTagsToSend(res);
         return { ...e, isClicked: true };
       })
     );
   };
 
   const addTag = (e) => {
-    const res = tags;
+    const res = tagsToSend;
     if (e.key === 'Enter' && text.length) {
       console.log(text);
       res.push({ id: `${res.length}${text}`, text: text, isClicked: true });
-      setTag(res);
+      setTagsToSend(res);
       setText('');
     }
   };
 
   return (
     <>
-      <h2>hello</h2>
-
       <div className="block1">
-        {tags.map((e) => {
-          return e.isClicked ? (
-            <Chip key={e.id} label={e.text} onDelete={handleDelete} />
-          ) : null;
-        })}
+        <div ref={divRef}>
+          {tagsToSend.map((e) => {
+            return e.isClicked ? (
+              <Chip key={e.id} label={e.text} onDelete={handleDelete} />
+            ) : null;
+          })}
+        </div>
 
         <input
           type="text"
@@ -65,12 +87,17 @@ function App() {
           className="myInput"
           onKeyUp={(e) => addTag(e)}
           value={text}
+          ref={inputRef}
         />
       </div>
       <ul>
         {tags.map((e) => {
           return (
-            <li key={e.id} onClick={() => onLiClick(e.id)}>
+            <li
+              key={e.id}
+              onClick={() => onLiClick(e.id)}
+              className={e.isClicked ? 'clicked' : null}
+            >
               {e.text}
             </li>
           );
